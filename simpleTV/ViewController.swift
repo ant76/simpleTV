81bcf7d4fcf6df9c1fa4b1bcf91e29ad7b12bbe2
trusov.andrew <trusov.andrew@gmail.com>
2015-12-16 00:33:45 +0400

    Initial Commit

---
+++ simpleTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleTV
+//
+//  Created by user on 16.12.15.
+//  Copyright © 2015 Trusov Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
